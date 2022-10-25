7f8a3f8fd5397df9afbed267b57ac741e1196775
Joti <tanjilajoti@gmail.com>
2022-10-25 13:01:19 +0600

    apply server

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSideNav = () => {
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/courses')
+            .then(res => res.json())
+            .then(data => setCourses(data));
+    }, [])
     return (
         <div>
-            <h2>Left</h2>
+            <h4>All Category: {courses.length}</h4>
+            {
+                courses.map(course => <p key={course.id}>
+                    <Link to={`/course/${course.id}`}>{course.name}</Link>
+                </p>)
+            }
 
         </div>
     );
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
+import Blogs from "../../Pages/Blogs/Blogs";
 import Courses from "../../Pages/Courses/Courses";
+import FAQ from "../../Pages/FAQ/FAQ";
 import Home from "../../Pages/Home/Home";
 
 export const routes = createBrowserRouter([
@@ -19,6 +21,16 @@ export const routes = createBrowserRouter([
                 element: <Courses></Courses>,
                 //  loader: () => fetch('https://dragon-news-server-seven.vercel.app/news')
             },
+            {
+                path: '/blogs/:id',
+                element: <Blogs></Blogs>,
+                //  loader: () => fetch('https://dragon-news-server-seven.vercel.app/news')
+            },
+            {
+                path: '/faq/:id',
+                element: <FAQ></FAQ>,
+                //  loader: () => fetch('https://dragon-news-server-seven.vercel.app/news')
+            }
 
 
         ]
