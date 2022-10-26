21e503a96151a19415fa6a56a57619aa5760d9de
Joti <tanjilajoti@gmail.com>
2022-10-26 08:29:48 +0600

    added enviroment

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBfinld3qICjfA3lTgezhoHwskPcFVrmwk",
-    authDomain: "learn-concepts-6e4b9.firebaseapp.com",
-    projectId: "learn-concepts-6e4b9",
-    storageBucket: "learn-concepts-6e4b9.appspot.com",
-    messagingSenderId: "294259787771",
-    appId: "1:294259787771:web:8957b2f220566d84cced5e",
-    measurementId: "G-ZZH8Z5S22D"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
 };
 
 // Initialize Firebase
