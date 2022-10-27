e7d7b901e39da68da658a41c721e72e25639a755
Joti <tanjilajoti@gmail.com>
2022-10-27 15:41:20 +0600

    add enviroment

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBfinld3qICjfA3lTgezhoHwskPcFVrmwk",
+    apiKey: process.env.REACT_APP_apiKey,
     authDomain: process.env.REACT_APP_authDomain,
     projectId: process.env.REACT_APP_projectId,
     storageBucket: process.env.REACT_APP_storageBucket,
